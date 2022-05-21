d5f3213ee3388e373113aa032834e2c4f94c8e58
jxbx <bailey.justin@gmail.com>
2022-05-21 22:14:40 +0100

    refactored render() and generateCss functions

--- patternator1.js
+++ patternator1.js
@@ -3,6 +3,7 @@ const bigContainer = document.getElementById("bigContainer");
 const gridItems = document.getElementsByClassName("item");
 const box = document.getElementById("box");
 const alternating = document.getElementById("alternating");
+const spacing = 300;
 
 let coordinates = [];
 
@@ -14,10 +15,13 @@ let state = {
   zoom: null,
   angle: null,
   alternating: null,
-  colour0: null,
-  colour1: null,
-  colour2: null,
+  color0: null,
+  color1: null,
+  color2: null,
 }
+
+
+
 
 function generateCoordinates() {
   let min = -150;
@@ -54,66 +58,59 @@ zoom.addEventListener("input", render);
 const characterInput = document.getElementById("characterInput");
 characterInput.addEventListener("input", render);
 
-const parent0 = document.getElementById('parent0');
 const background = document.getElementById('wrapper');
 
-const picker0 = new Picker({
-  parent: parent0,
-  color: "#2197ac",
-  popup: "top",
-  alpha: false,
-  onChange: function (color) {
-    smallContainer.style.backgroundColor = color.rgbaString;
-    parent0.style.backgroundColor = color.rgbaString;
-    }
-  });
 
-const parent1 = document.getElementById("parent1");
-const picker1 = new Picker({
-  parent: parent1,
-  color: "#f45555ff",
-  popup: "top",
-  onChange: function (color) {
+function generateCss() {
+
+    while (smallContainer.hasChildNodes()) {
+      smallContainer.removeChild(smallContainer.firstChild);
+    }
+
+    coordinates = [];
+    generateCoordinates();
+
+    for (let i=0; i<9; i++){
+      const shape = document.createElementNS('http://www.w3.org/2000/svg',"text");
+      shape.setAttributeNS(null, "id", "item"+i);
+      shape.setAttributeNS(null, "x", coordinates[i][0]);
+      shape.setAttributeNS(null, "y", coordinates[i][1]);
+      shape.setAttributeNS(null, "class", "item");
+      const text  = document.createTextNode(characterInput.value);
+      shape.appendChild(text);
+      smallContainer.appendChild(shape);
+    }
+
     for (const item of gridItems){
-      item.style.fill = color.rgbaString;
-    }
-    parent1.style.backgroundColor = color.rgbaString;
-    }
-  });
+      state.dropShadow <= 0 ? item.style.filter = null : item.style.filter = "drop-shadow("+ state.dropShadow +"px 0 " + state.color2 + ")";
 
-  const parent2 = document.getElementById("parent2");
-  const picker2 = new Picker({
-    parent: parent2,
-    color: "#356969ff",
-    popup: "top",
-    onChange: function (color) {
-      for (const item of gridItems){
-          dropShadow.value <= 0 ? item.style.filter = null : item.style.filter = "drop-shadow("+dropShadow.value+"px 0" + color.hex +")";
-      };
-      parent2.style.backgroundColor = color.rgbaString;
+      item.style.fontSize = state.fontSize + "px";
+      item.style.fontWeight = state.fontWeight;
+      item.style.fill = state.color1;
+      item.style.transformBox = "fill-box";
+      item.style.transformOrigin = "center";
+      item.style.textAnchor = "middle";
+      item.style.dominantBaseline = "middle";
+      item.style.fontFamily = "\"Raleway\", sans-serif";
+    }
+    for (let i=0; i<9; i++){
+      if (alternating.checked && i%2 === 0){
+        gridItems[i].style.transform = null;
       }
-    });
+      else {
+        gridItems[i].style.transform = "rotate("+angle.value+"deg)";
+      }
+    }
+
+
+  smallContainer.setAttributeNS(null, "viewBox", -0.5*spacing + " " + -0.5*spacing + " " + spacing + " " + spacing);
+
+  let cssReady = "background-image: url(\'data:image/svg+xml;utf8," + smallContainer.outerHTML + "\'); background-size: " + zoom.value + "px;";
+
+  return cssReady;
+}
 
 function render() {
-
-  while (smallContainer.hasChildNodes()) {
-    smallContainer.removeChild(smallContainer.firstChild);
-  }
-
-  coordinates = [];
-  generateCoordinates();
-
-
-  for (let i=0; i<9; i++){
-    const shape = document.createElementNS('http://www.w3.org/2000/svg',"text");
-    shape.setAttributeNS(null, "id", "item"+i);
-    shape.setAttributeNS(null, "x", coordinates[i][0]);
-    shape.setAttributeNS(null, "y", coordinates[i][1]);
-    shape.setAttributeNS(null, "class", "item");
-    const text  = document.createTextNode(characterInput.value);
-    shape.appendChild(text);
-    smallContainer.appendChild(shape);
-  }
 
   dropShadowValue.innerHTML = dropShadow.value;
   fontSizeValue.innerHTML = fontSize.value;
@@ -121,38 +118,67 @@ function render() {
   zoomValue.innerHTML = zoom.value;
   angleValue.innerHTML = angle.value;
 
-  for (const item of gridItems){
-    dropShadow.value <= 0 ? item.style.filter = null : item.style.filter = "drop-shadow("+dropShadow.value+"px 0 " +picker2.color.rgbaString+")";
-    item.style.fontFamily = "\"Raleway\", sans-serif";
-    item.style.fontSize = fontSize.value + "px";
-    item.style.fontWeight = fontWeight.value;
-    item.style.fill = picker1.color.rgbaString;
-    item.style.transformBox = "fill-box";
-    item.style.transformOrigin = "center";
-    item.style.textAnchor = "middle";
-    item.style.dominantBaseline = "middle";
-  }
-  for (let i=0; i<9; i++){
-    if (alternating.checked && i%2 === 0){
-      gridItems[i].style.transform = null;
-    }
-    else {
-      gridItems[i].style.transform = "rotate("+angle.value+"deg)";
-    }
-  }
+  state.character = characterInput.value;
+  state.angle = angle.value;
+  state.dropShadow = dropShadow.value;
+  state.fontSize = fontSize.value;
+  state.fontWeight = fontWeight.value;
+  state.zoom = zoom.value;
+  state.alternating = alternating.checked;
 
-//  let newSpacing = (alternating.checked) ? 300 : 150
+  // state.colour0 = picker0.color.rgbaString;
+  // state.colour1 = picker1.color.rgbaString;
+  // state.colour2 = picker2.color.rgbaString;
 
-let newSpacing = 300;
+  let cssReady = generateCss();
 
-smallContainer.setAttributeNS(null, "viewBox", -0.5*newSpacing + " " + -0.5*newSpacing + " " + newSpacing + " " + newSpacing);
-
-  let cssReady = "background-image: url(\'data:image/svg+xml;utf8," + smallContainer.outerHTML + "\'); background-size: " + zoom.value + "px;";
-
- document.body.setAttributeNS(null, "style", cssReady);
-
+ document.body.setAttribute("style", cssReady);
 }
 
+
+const parent0 = document.getElementById('parent0');
+const picker0 = new Picker({
+  parent: parent0,
+  color: "#2197ac",
+  popup: "bottom",
+  alpha: false,
+  onChange: function (color) {
+    smallContainer.style.backgroundColor = color.rgbaString;
+    parent0.style.backgroundColor = color.rgbaString;
+    state.color0 = color.rgbaString;
+    render();
+    }
+  });
+
+const parent1 = document.getElementById("parent1");
+const picker1 = new Picker({
+  parent: parent1,
+  color: "#f45555ff",
+  popup: "bottom",
+  onChange: function (color) {
+    for (const item of gridItems){
+      item.style.fill = color.rgbaString;
+    }
+    parent1.style.backgroundColor = color.rgbaString;
+    state.color1 = color.rgbaString;
+    render();
+    }
+  });
+
+  const parent2 = document.getElementById("parent2");
+  const picker2 = new Picker({
+    parent: parent2,
+    color: "#356969ff",
+    popup: "bottom",
+    onChange: function (color) {
+      for (const item of gridItems){
+          dropShadow.value <= 0 ? item.style.filter = null : item.style.filter = "drop-shadow("+dropShadow.value+"px 0" + color.hex +")";
+      };
+      parent2.style.backgroundColor = color.rgbaString;
+      state.color2 = color.rgbaString;
+      render();
+      }
+    });
 
 
 const randomiseParameters = document.getElementById("randomiseParameters");
@@ -169,23 +195,23 @@ randomiseParameters.onclick = function() {
   render();
 }
 
-const randomiseColours = document.getElementById("randomiseColours");
+const randomiseColors = document.getElementById("randomiseColors");
 
-randomiseColours.onclick = function() {
-  function newColour () {
+randomiseColors.onclick = function() {
+  function newColor () {
     let letters = '0123456789ABCDEF'.split('');
-    let colour = '#';
+    let color = '#';
     for (let i = 0; i < 6; i++ ) {
-      colour += letters[Math.round(Math.random() * 15)];
+      color += letters[Math.round(Math.random() * 15)];
     }
-    return colour;
+    return color;
   }
 
-  //setColour() is from the vanillapicker module;
+  //setColor() is from the vanillapicker module;
 
-  picker0.setColour(newColour());
-  picker1.setColour(newColour());
-  picker2.setColour(newColour());
+  picker0.setColor(newColor());
+  picker1.setColor(newColor());
+  picker2.setColor(newColor());
 
   render();
 }
@@ -193,22 +219,14 @@ randomiseColours.onclick = function() {
 const randomiseEverything = document.getElementById("randomiseEverything");
 
 randomiseEverything.onclick = function() {
-  randomiseColours.click();
+  randomiseColors.click();
   randomiseParameters.click();
 }
 
 const shareButton = document.getElementById("shareButton");
-shareButton.onclick = function setState() {
-  state.character = characterInput.value;
-  state.angle = angle.value;
-  state.dropShadow = dropShadow.value;
-  state.fontSize = fontSize.value;
-  state.fontWeight = fontWeight.value;
-  state.zoom = zoom.value;
-  state.alternating = alternating.checked;
-  state.colour0 = picker0.color.rgbaString;
-  state.colour1 = picker1.color.rgbaString;
-  state.colour2 = picker2.color.rgbaString;
+
+shareButton.onclick = function updateURL() {
+
 
   let newState =  JSON.stringify(state);
   window.location.hash = newState;
@@ -229,23 +247,16 @@ function loadState() {
   else {
     state = JSON.parse(decodeURIComponent(string));
     characterInput.value = state.character;
-    characterInput.dispatchEvent(new Event('input'));
     angle.value = state.angle;
-    angle.dispatchEvent(new Event('input'));
     dropShadow.value = state.dropShadow;
-    dropShadow.dispatchEvent(new Event('input'));
     fontSize.value = state.fontSize;
-    fontSize.dispatchEvent(new Event('input'));
     fontWeight.value = state.fontWeight;
-    fontWeight.dispatchEvent(new Event('input'));
-    zoom.value = state.spacing;
-    zoom.dispatchEvent(new Event('input'));
+    zoom.value = state.zoom;
     alternating.checked = state.alternating;
-    alternating.dispatchEvent(new Event('change'));
 
-    picker0.setColour(state.colour0);
-    picker1.setColour(state.colour1);
-    picker2.setColour(state.colour2);
+    picker0.setColor(state.color0);
+    picker1.setColor(state.color1);
+    picker2.setColor(state.color2);
 
     render();
   }
@@ -254,22 +265,29 @@ function loadState() {
 const downloadSVGButton = document.getElementById("downloadSVG");
 
 
-function downloadSVG() {
-  const blob = new Blob([bigContainer.innerHTML], { type: "image/svg+xml" });
-  const objectUrl = URL.createObjectURL(blob);
+// function downloadSVG() {
+//   const blob = new Blob([bigContainer.innerHTML], { type: "image/svg+xml" });
+//   const objectUrl = URL.createObjectURL(blob);
+//
+//   const link = document.createElement("a");
+//   link.href = objectUrl;
+//   link.download = "mypattern";
+//   document.body.appendChild(link);
+//   link.click();
+//   document.body.removeChild(link);
+//   setTimeout(() => URL.revokeObjectURL(objectUrl), 5000);
+// }
 
-  const link = document.createElement("a");
-  link.href = objectUrl;
-  link.download = "mypattern";
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-  setTimeout(() => URL.revokeObjectURL(objectUrl), 5000);
+const cssRepeat = document.getElementById("cssRepeat")
+cssRepeat.onclick = function () {
+  navigator.clipboard.writeText(generateCss())
+  .then(()=>{
+    alert("CSS copied to clipboard");
+  })
+  .catch(()=>{
+    alert("error");
+  })
 }
-
-// const cssRepeat = document.getElementById("cssRepeat")
-//  cssRepeat.onclick = function generatePatternRepeat() {
-
 
 
 
